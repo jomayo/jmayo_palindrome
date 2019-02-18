3daec5b76d2dbac24f637955eae6aac87207a916
john mayo <mayo.rides@gmail.com>
2019-02-18 23:51:09 +0000

    Initialize repository

---
+++ test/spec/pali_spec.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+describe JmayoPalindrome do
+  it "test_it_does_something_useful" do
+    
+  end
+end
+  
+describe JmayoPalindrome do
+	it "test_something_else" do
+	  
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,5 @@
+$LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
+require "jmayo_palindrome"
+require "minitest/autorun"
+require "minitest/reporters"
+Minitest::Reporters.use!
