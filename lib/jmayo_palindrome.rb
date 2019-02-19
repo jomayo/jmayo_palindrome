029d20064c970839c785f1f62ad788c19a7c7c45
john mayo <mayo.rides@gmail.com>
2019-02-19 18:06:35 +0000

    Finish working and refactored palindrome method

--- lib/jmayo_palindrome.rb
+++ lib/jmayo_palindrome.rb
@@ -1,8 +1,34 @@
 require "jmayo_palindrome/version"
 
-module Jmayo
-  module Palindrome 
-    
-  # Your code goes here...
+class String
+  #Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+  
+  #Returns the letters in a string
+  def letters
+  # #before refactoring with functional programming
+  #   the_letters = []
+  #   letter_regex = /[a-z]/i
+  #   self.chars.each do |character|
+  #     if character.match(letter_regex)
+  #       the_letters << character
+  #     end
+  #   end
+  #   the_letters.join
+  
+    self.chars.select { |c| c.match(/[a-z]/i) }.join #after refactoring :-)
+  
+  end
+  
+  private
+  
+  #Returns content for palindrome testing.
+  def processed_content
+    self.scan(/[a-z]/i).join.downcase
   end
 end
+
+
+
